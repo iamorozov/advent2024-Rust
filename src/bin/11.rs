a845f48e32ef0e257c6fd92811d4984d9f819de3
iamorozov <ivan.morozov@deliveroo.co.uk>
2024-12-11 11:15:16 +0000

    day11

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,24 +1,48 @@
 use itertools::Itertools;
+use memoize::memoize;
 
 advent_of_code::solution!(11);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let mut stones = input.trim().split_whitespace().map(|n| n.parse::<u64>().unwrap()).collect_vec();
+pub fn part_one(input: &str) -> Option<u64> {
+    let stones = input.trim().split_whitespace().map(|n| n.parse::<u64>().unwrap()).collect_vec();
 
-    for blink in 0..6 {
-        let copy = stones.iter().copied().collect_vec();
-        for (i, stone) in stones.iter_mut().enumerate() {
-            if *stone == 0 {
-                stones[i] = 1;
-            }
-        }
-    }
+    let result = stones.iter().map(|&stone| count_splits(stone, 25)).sum::<u64>();
 
-    Some(0)
+    Some(result)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+#[memoize]
+fn count_splits(stone: u64, blinks: u64) -> u64 {
+    if blinks == 0 {
+        return 1;
+    }
+
+    if stone == 0 {
+        return count_splits(1, blinks - 1);
+    }
+
+    if stone.to_string().len() % 2 == 0 {
+        let (left, right) = split(stone);
+        return count_splits(left, blinks - 1) + count_splits(right, blinks - 1);
+    } else {
+        return count_splits(stone * 2024, blinks - 1);
+    }
+}
+
+fn split(stone: u64) -> (u64, u64) {
+    let str = stone.to_string();
+    let left = str[0..(str.len() / 2)].parse::<u64>().unwrap();
+    let right = str[(str.len() / 2)..].parse::<u64>().unwrap();
+
+    (left, right)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let stones = input.trim().split_whitespace().map(|n| n.parse::<u64>().unwrap()).collect_vec();
+
+    let result = stones.iter().map(|&stone| count_splits(stone, 75)).sum::<u64>();
+
+    Some(result)
 }
 
 #[cfg(test)]
@@ -28,12 +52,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(22));
+        assert_eq!(result, Some(55312));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(65601038650482));
     }
 }
