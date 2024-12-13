dbcdc407237fca3e2f1e505533ed52eab01b3a59
iamorozov <ivan.morozov@deliveroo.co.uk>
2024-12-13 15:18:26 +0000

    day13

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -1,15 +1,11 @@
-use std::cmp::min;
-use memoize::memoize;
-
 use itertools::Itertools;
 advent_of_code::solution!(13);
 
 pub fn part_one(input: &str) -> Option<u64> {
     let tokens = extract_machines(input, 0)
         .into_iter()
-        .map(|(ax, ay, bx, by, prize_x, prize_y)| calculate_tokens(ax, ay, bx, by, prize_x, prize_y))
-        .flatten()
-        .sum();
+        .map(|(ax, ay, bx, by, prize_x, prize_y)| solve(ax, ay, bx, by, prize_x, prize_y))
+        .sum::<i64>() as u64;
 
     Some(tokens)
 }
@@ -24,36 +20,12 @@ fn extract_machines(input: &str, correction: i64) -> Vec<(i64, i64, i64, i64, i6
             let (bx, by) = sscanf::scanf!(machine[1], "Button B: X+{}, Y+{}", i64, i64).unwrap();
             let (prize_x, prize_y) = sscanf::scanf!(machine[2], "Prize: X={}, Y={}", i64, i64).unwrap();
 
-            // println!("A ({}, {}) -> B ({}, {}) -> Prize ({}, {})", ax, ay, bx, by, prize_x, prize_y);
-
             (ax, ay, bx, by, prize_x + correction, prize_y + correction)
         })
         .collect_vec()
 }
 
-#[memoize]
-fn calculate_tokens(ax: i64, ay: i64, bx: i64, by: i64, prize_x: i64, prize_y: i64) -> Option<u64> {
-    // println!("A ({}, {}) -> B ({}, {}) -> Prize ({}, {})", ax, ay, bx, by, prize_x, prize_y);
-
-    if prize_x == 0 && prize_y == 0 {
-        return Some(0);
-    }
-    if prize_x < 0 || prize_y < 0 {
-        return None;
-    }
-
-    let press_a = calculate_tokens(ax, ay, bx, by, prize_x - ax, prize_y - ay);
-    let press_b = calculate_tokens(ax, ay, bx, by, prize_x - bx, prize_y - by);
-
-    match (press_a, press_b) {
-        (Some(a), Some(b)) => Some(min(3 + a, 1 + b)),
-        (Some(a), None) => Some(3 + a),
-        (None, Some(b)) => Some(1 + b),
-        (None, None) => None,
-    }
-}
-
-fn solve_machine(ax: i64, ay: i64, bx: i64, by: i64, prize_x: i64, prize_y: i64) -> i64 {
+fn solve(ax: i64, ay: i64, bx: i64, by: i64, prize_x: i64, prize_y: i64) -> i64 {
     let prize = (prize_x, prize_y);
     let det = ax * by - ay * bx;
     let a = (prize.0 * by - prize.1 * bx) / det;
@@ -68,7 +40,7 @@ fn solve_machine(ax: i64, ay: i64, bx: i64, by: i64, prize_x: i64, prize_y: i64)
 pub fn part_two(input: &str) -> Option<u64> {
     let tokens = extract_machines(input, 10000000000000)
         .into_iter()
-        .map(|(ax, ay, bx, by, prize_x, prize_y)| solve_machine(ax, ay, bx, by, prize_x, prize_y))
+        .map(|(ax, ay, bx, by, prize_x, prize_y)| solve(ax, ay, bx, by, prize_x, prize_y))
         .sum::<i64>() as u64;
 
     Some(tokens)
